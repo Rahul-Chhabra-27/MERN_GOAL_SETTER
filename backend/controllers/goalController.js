1bca9187b362453213034ada09add8cb1ea0bf21
Rahul Chhabra <chhabrarahul2707@gmail.com>
2022-06-26 13:51:19 +0530

    Initial REST API and CRUD functionality

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,11 @@
 const asyncHanlder = require('express-async-handler');
+const goalModel = require('../model/goalModel');
 // @desc        Get goals
 // @Route       GET  /api/goals
 // @access      Private
 const getGoals = asyncHanlder(async(req,res) => {
-    res.json({mssg:"getGoals"})
+    const goals = await goalModel.find({}).sort({createdAt:-1});
+    res.status(200).json(goals);
 })
 // @desc        Post goal
 // @Route       POST  /api/goals
@@ -12,25 +14,45 @@ const setGoal = asyncHanlder(async(req,res) => {
     if(!req.body.text) {
         throw new Error('please provide text field');
     }
-    res.json({mssg:"setGoals"});
+    const goal = await goalModel.create({...req.body});
+    if(!goal) throw new Error('Goal is not added...');
+    res.status(200).json(goal);
 })
 // @desc        Update goal
 // @Route       PUT  /api/goals/:id
 // @access      Private
 const updateGoal = asyncHanlder(async(req,res) => {
-    res.json({mssg:"updateGoal"});
+    const { id } = req.params;
+    const goal = await goalModel.findOneAndUpdate({_id:id},{...req.body});
+    if(!goal) {
+        res.status(400);
+        throw new Error('Goal is not updated!!');
+    }
+    res.status(200).json(goal);
 })
 // @desc        Delete goal
 // @Route       DELETE  /api/goals/:id
 // @access      Private
 const deleteGoal = asyncHanlder(async(req,res) => {
-    res.json({mssg:"deleteGoal"});
+    const { id } = req.params;
+    const goal = await goalModel.findOneAndDelete({_id:id});
+    if(!goal) {
+        res.status(400);
+        throw new Error('Goal is not deleted successfully...');
+    }
+    res.status(200).json(goal);
 })
 // @desc        Get goal
 // @Route       GET  /api/goals/:id
 // @access      Private
 const getGoal = asyncHanlder(async(req,res) => {
-    res.json({mssg:"getGoal"});
+    const { id } = req.params;
+    const goal = await goalModel.find({ _id: id });
+    if(!goal) {
+        res.status(400);
+        throw new Error('Goal not found!!');
+    }
+    res.status(200).json(goal);
 })
 module.exports = {
     getGoal,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const goalRoutes = require('./routes/goalRoutes');
+require('colors');
 const { errorHandler } = require('./middleware/errorHandler');
+const { connectDB } = require('./config/db');
 
 dotenv.config();
+connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
@@ -12,4 +15,4 @@ app.use(express.urlencoded({ extended: false}));
 app.use('/api/goals',goalRoutes);
 app.use(errorHandler);
 const port = process.env.PORT || 5000;
-app.listen(port,() => console.log(`Server is ruuning in ${process.env.NODE_ENV} mode on port ${port}`))
+app.listen(port,() => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${port}`.white.underline))
